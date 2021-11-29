da9854e580fcd54846ac40b3f4c57df821ebf50d
LanhNguyen <lanhnguyen@LanhNguyens-MacBook-Pro.local>
2021-11-29 21:44:35 +0700

    feat: logout

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,18 @@
+import Cookies from "js-cookie";
 import React from "react";
-
+import { StyleLogout } from "./styled";
 const Dashboard = () => {
-  return <div>Bạn đã đăng nhập thành công</div>;
+  const onLogout = () => {
+    Cookies.remove("token");
+    localStorage.removeItem("token");
+    window.location.href = "/login";
+  };
+  return (
+    <div>
+      <span>Bạn đã đăng nhập thành công</span>
+      <StyleLogout onClick={onLogout}>Đăng xuất</StyleLogout>
+    </div>
+  );
 };
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyleLogout = styled.div`
+  margin-top: 10px;
+  color: #1c1ce3;
+  cursor: pointer;
+  
+  &:hover {
+    text-decoration: underline;
+  }
+`;
